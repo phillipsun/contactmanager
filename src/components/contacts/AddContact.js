430e7c25840338b8d8d693afba8ed99c0672d868
Phillip Sun <psun.04@gmail.com>
2019-01-14 22:58:14 -0600

    added addcontact form to add a new contact

---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+
+class AddContact extends Component {
+  state = {
+    name: "",
+    email: "",
+    phone: ""
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    const { name, email, phone } = this.state;
+    return (
+      <div className="card mb-3">
+        <div className="card-header">
+          <h1>Add Contact</h1>
+        </div>
+        <div className="card-body">
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                name="name"
+                placeholder="Enter Name..."
+                value={name}
+                onChange={this.onChange}
+              />
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control form-control-lg"
+                name="email"
+                placeholder="Enter Email..."
+                value={email}
+                onChange={this.onChange}
+              />
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                name="phone"
+                placeholder="Enter Phone..."
+                value={phone}
+                onChange={this.onChange}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Add Contact"
+              className="btn btn-block btn-light"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddContact;
